27823d495a0cd5ed113baf5a56f8fadbb075222d
alex-mcrandal <mcrandalalex@gmail.com>
2023-09-05 13:52:10 -0400

    Created controller setup

---
+++ controller.py
@@ -0,0 +1,26 @@
+from model import TicTacToeModel
+
+'''
+This is the controller for the Tic Tac Toe game.
+It handles the exchange of information between the
+console (View) and gameboard (Model)
+'''
+
+def start_game():
+    game = TicTacToeModel()
+    while game.winner == '':
+        # TODO: print board
+        # TODO: prompt for spot to mark
+        # TODO: mark grid space
+    
+    # TODO: Congradulate winner
+
+def main():
+    do_play = True
+    while do_play:
+        start_game()
+        do_continue = input("Do you wish to play again? (y/n): ")
+        do_player = do_continue == 'y'
+
+if __name__ == "__main__":
+    main()
